effbdd5429c0e77a48d1a86db331fe050ea4d33f
jannick vb <jannick vb>
2017-10-20 12:36:51 +0200

    add databasehelper

--- app/src/main/java/com/avans/airportapp/data/local/AirportDBHelper.java
+++ app/src/main/java/com/avans/airportapp/data/local/AirportDBHelper.java
@@ -1,5 +1,27 @@
 package com.avans.airportapp.data.local;
 
 
-public class AirportDBHelper {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class AirportDBHelper extends SQLiteAssetHelper {
+
+    private static final String DB_NAME = "databases/airports.sqlite";
+    private static final int DB_VERSION = 1;
+
+    public AirportDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public Cursor getAirports() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        return null;
+    }
+
 }
