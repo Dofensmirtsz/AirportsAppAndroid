85daa1d92c147c3f31fe6a3864c1075c29e9951a
jannick vb <jannick vb>
2017-10-24 13:12:15 +0200

    add flagkit and fix recyclerview onclickedlistener

--- app/src/main/java/com/avans/airportapp/ui/detail/DetailActivity.java
+++ app/src/main/java/com/avans/airportapp/ui/detail/DetailActivity.java
@@ -2,19 +2,24 @@ package com.avans.airportapp.ui.detail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.avans.airportapp.R;
 import com.avans.airportapp.data.DataManager;
 import com.avans.airportapp.data.local.AirportDBHelper;
 import com.avans.airportapp.data.model.Airport;
+import com.jwang123.flagkit.FlagKit;
 
 import timber.log.Timber;
 
 public class DetailActivity extends AppCompatActivity implements DetailView {
 
     private DetailPresenter presenter;
+    private ImageView imageISO;
 
     public static Intent getStartIntent(Context context, String icao) {
         Intent intent = new Intent(context, DetailActivity.class);
@@ -27,6 +32,8 @@ public class DetailActivity extends AppCompatActivity implements DetailView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        imageISO = (ImageView) findViewById(R.id.image_iso_flag);
+
         presenter = new DetailPresenter(DataManager.instance(this), this);
         presenter.loadAirport(
                 getIntent().getStringExtra(AirportDBHelper.ICAO)
@@ -36,5 +43,10 @@ public class DetailActivity extends AppCompatActivity implements DetailView {
     @Override
     public void showAirport(Airport airport) {
         Timber.d(airport.toString());
+        try {
+            imageISO.setImageDrawable(FlagKit.drawableWithFlag(this, airport.getIsoCountry().toLowerCase()));
+        } catch (Resources.NotFoundException e) {
+            imageISO.setBackgroundColor(ContextCompat.getColor(this, R.color.colorSectionBackground));
+        }
     }
 }
